7d0847de42cbf4aa671ca4adc8e75e998369b352
Parth <parthwani22@gmail.com>
2025-12-03 23:49:28 +0530

    Js function to search and return query done.

--- travelRecommendation.js
+++ travelRecommendation.js
@@ -1,29 +1,84 @@
 (function () {
     // Get Elements:
-    const searchBar = document.getElementById('serch-bar');
+    const searchBar = document.getElementById('search-bar');
     const searchButton = document.getElementById('search-button');
     const resetButton = document.getElementById('reset-search-button');
 
     // Event Listeners
-    const debouncedRender = debounce(searchData, 200);
-    searchBar.addEventListener('input', debouncedRender);
     searchButton.addEventListener('click', searchData);
     resetButton.addEventListener('click', resetSearch);
 
-    // Debouced Reader function
-    function debounce(func, delay) {
-        let timeout;
-        return () => {
-            clearInterval(timeout);
-            timeout = setTimeout(func, delay);
-        };
-    };
     // Function to search the given keyword from the json file
     function searchData () {
-        const searchQuery = searchBar.value;
+        const searchQuery = searchBar.value.toLowerCase();
+        // console.log(searchQuery);  // Debug log
+        if(searchQuery === '') {
+            return;
+        };
+        let resultHtml = '';
         fetch('./travel_recommendation_api.json')
-            .then(resp => {
-                const data = resp.json();
+            .then(async resp => {
+                const data = await resp.json();
+                // Search Match
+                let keySearchResult;
+                Object.keys(data).forEach(key => {
+                    if(key.includes(searchQuery)) {
+                        keySearchResult = key;
+                    };
+                });
+                // console.log(keySearchResult);  // Debug log
+                if(keySearchResult) {
+                    const placeQuery = data[keySearchResult];
+                    // console.log(placeQuery)  // Debug log
+                    switch (keySearchResult) {
+                        case 'countries':
+                            for(const country of placeQuery) {
+                                resultHtml += `
+                                    <div id="searchResult">
+                                        <figure>
+                                            <img src="${country.cities[0].imageUrl}" height="100px">
+                                        </figure>
+                                        <h2>${country.name}</h2>
+                                        <h3>${country.cities[0].name}</h3>
+                                        <p>${country.cities[0].description}</p>
+                                    </div>
+                                `;
+                            };
+                            break;
+                        
+                        case 'temples':
+                            for(const temple of placeQuery) {
+                                resultHtml += `
+                                    <div id="searchResult">
+                                        <figure>
+                                            <img src="${temple.imageUrl}" height="100px">
+                                        </figure>
+                                        <h2>${temple.name}</h2>
+                                        <p>${temple.description}</p>
+                                    </div>
+                                `;
+                            };
+                            break;
+                        case 'beaches':
+                            for(const beach of placeQuery) {
+                                resultHtml += `
+                                    <div id="searchResult">
+                                        <figure>
+                                            <img src="${beach.imageUrl}" height="100px">
+                                        </figure>
+                                        <h2>${beach.name}</h2>
+                                        <p>${beach.description}</p>
+                                    </div>
+                                `;
+                            };
+                            break;
+                        default:
+                            break;
+                    };
+                } else {
+                    resultHtml = `<p>No data found</p>`;
+                };
+                // console.log(resultHtml);  // Debug log
                 // Get serchQuery from data
                 // Display on screen
             })
